7bbac275d50d1393c00eff322ab7313401ff3514
Frank-Colo <franromerobaulies@gmail.com>
2026-06-01 22:46:56 -0300

    Add files via upload

---
+++ Backend/TDAs/Java Cola/ICola.java
@@ -0,0 +1,11 @@
+//ICola
+public interface ICola {
+    public void encolar(int elemento);
+    public int desencolar();
+    public boolean estaVacia();
+    public boolean estaLlena();
+    public int frente();
+    public int finalCola();
+    public int tamanio();
+    public void mosrtrar();
+}
