d85383803b70143254bd8fb14711bca8cb590116
Frank-Colo <franromerobaulies@gmail.com>
2026-06-01 22:38:21 -0300

    Delete Backend/TDAs/Java Diccionario directory

--- Backend/TDAs/Java Diccionario/IDiccionario.java
+++
@@ -1,8 +0,0 @@
-public interface IDiccionario {
-    public void insertar(String clave, int valor);
-    public int obtener(String clave);
-    public int eliminar(String clave);
-    public boolean estaVacio();
-    public int tamanio();
-    public void mostrar();
-}
